6d56ba0787b491d38f5b065f8cc02ce47bc176c7
Nick Hobart <nick.hobart@cision.com>
2015-03-16 09:23:18 -0500

    inital commit with a super basic script

---
+++ aws.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## This installs the lastest AWS CLI tools ##
+## You need 'sudo' access to make this work. ##
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
