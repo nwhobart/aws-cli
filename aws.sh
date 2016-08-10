ea851aefc51bc8d756aeb750dc0e3767bec2a55b
Nick Hobart <nickhobart@gmail.com>
2016-08-10 17:41:16 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -2,6 +2,7 @@
 
 ## This installs the lastest AWS CLI tools ##
 ## You need 'sudo' access to make this work. ##
+## travis commit##
 
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
